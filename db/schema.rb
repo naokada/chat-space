e828e3158a4ebd3c742f9399da5a39895fba45eb
LAPTOP-KUD45KG1\naomi <nao.miwa.1112@gmail.com>
2017-07-01 20:32:44 +0900

    Revert "グループのモデル、コントローラーの作成"
    This reverts commit 9f042c0425df1a5c4309d08b83eb6cfe4d74ed77.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701111722) do
-
-  create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name",       null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20170624143756) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "name",                                null: false
