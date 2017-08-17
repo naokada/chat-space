bd3c67a52a4a2035a279dd652f056ec6565df774
vagrant <vagrant@vagrant-centos65.vagrantup.com>
2017-08-17 17:30:37 +0900

    editing-messagecontroller-for-flash

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,31 +1,33 @@
 class MessagesController < ApplicationController
+
+  before_action :find_message_group, only: [:new, :create]
+
   def index
     @groups = current_user.groups
   end
 
   def new
-    @group = find_message_group
-    @users = @group.users
     @message = Message.new
   end
 
   def create
-    @group = find_message_group
-    @message = Message.new(body: message_params[:body], image: message_params[:image], user_id: current_user.id, group_id: @group.id)
+    @message = current_user.messages.new(message_params)
     if @message.save
       redirect_to new_group_message_path(@group)
     else
-      redirect_to new_group_message_path(@group), alert: "メッセージを入力してください"
+      flash.now[:alert] = "メッセージを入力してください"
+      render :new
     end
   end
 
 private
   def message_params
-    params.require(:message).permit(:body, :image, :image_cache, :remove_image)
+    params.require(:message).permit(:body, :image, :image_cache, :remove_image).merge(group_id: params[:group_id])
   end
 
   def find_message_group
     @group = Group.find(params[:group_id])
+    @users = @group.users
   end
 
 end
