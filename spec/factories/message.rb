ee2a0da0204a14cbb863b2bfe02488efcbe0c1c0
LAPTOP-KUD45KG1\naomi <nao.miwa.1112@gmail.com>
2017-08-30 17:42:01 +0900

    refactoring message model test

--- spec/factories/message.rb
+++ spec/factories/message.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :message do
-    body  Faker::Name.name
+    body  Faker::Lorem.sentence
     image Rack::Test::UploadedFile.new(File.join(Rails.root, 'test/fixtures/files/sample.jpg'))
   end
 
--- test/models/message_spec.rb
+++ test/models/message_spec.rb
@@ -2,46 +2,83 @@ require 'rails_helper'
 
 class MessageTest < ActiveSupport::TestCase
   describe Message do
-    let(:group) {create(:create)}
-    let(:user) {create(:user)}
+    let(:group) { create(:create) }
+    let(:user) { create(:user) }
     before do
       @group = user.groups.first
     end
-
-    it "is valid with message with a body" do
-      message = build(:message, image: nil, user_id: user.id, group_id: @group.id)
-      message.valid?
-      expect(message).to be_valid
+    let(:message) do
+      message = build(:message, body: body, image: image, user_id: user_id, group_id: group_id)
     end
 
-    it "is valid with message with an image" do
-      message = build(:message, body: nil, user_id: user.id, group_id: @group.id)
-      message.valid?
-      expect(message).to be_valid
+    context "has group_id and user_id" do
+    let(:body) { nil }
+    let(:image) { nil }
+    let(:user_id) { user.id }
+    let(:group_id) { @group.id }
+
+      context "has body" do
+        let(:body) { Faker::Lorem.sentence }
+
+        it "is valid with message with a body" do
+          message.valid?
+          expect(message).to be_valid
+        end
+      end
+
+      context "has image" do
+        let(:image) { Rack::Test::UploadedFile.new(File.join(Rails.root, 'test/fixtures/files/sample.jpg')) }
+
+        it "is valid with message with an image" do
+          message.valid?
+          expect(message).to be_valid
+        end
+      end
+
+      context "has both body and image" do
+        let(:body) { Faker::Lorem.sentence }
+        let(:image) { Rack::Test::UploadedFile.new(File.join(Rails.root, 'test/fixtures/files/sample.jpg')) }
+
+        it "is valid with message with an image and a body" do
+          message.valid?
+          expect(message).to be_valid
+        end
+      end
     end
 
-    it "is valid with message with an image and a body" do
-      message = build(:message, user_id: user.id, group_id: @group.id)
-      message.valid?
-      expect(message).to be_valid
-    end
+    context "does not have a nessary factor" do
+      let(:body) { Faker::Lorem.sentence }
+      let(:image) { Rack::Test::UploadedFile.new(File.join(Rails.root, 'test/fixtures/files/sample.jpg')) }
+      let(:user_id) { user.id }
+      let(:group_id) { @group.id }
 
-    it "is invalid with message without neither an image or a body" do
-      message = build(:invalid_message, user_id: user.id, group_id: @group.id)
-      message.valid?
-      expect(message.errors[:body_or_image]).to include("を入力してください")
-    end
+      context "does not have neither an image or a body" do
+        let(:body) { nil }
+        let(:image) { nil }
 
-    it "is invalid with message without user_id" do
-      message = build(:message, user_id: nil, group_id: @group.id)
-      message.valid?
-      expect(message.errors[:user]).to include("を入力してください")
-    end
+        it "is invalid with message without neither an image or a body" do
+          message.valid?
+          expect(message.errors[:body_or_image]).to include("を入力してください")
+        end
+      end
 
-    it "is invalid with message without group_id" do
-      message = build(:message, user_id: user.id, group_id: nil)
-      message.valid?
-      expect(message.errors[:group]).to include("を入力してください")
+      context "does not have neither an user_id" do
+        let(:user_id) { nil }
+
+        it "is invalid with message without user_id" do
+          message.valid?
+          expect(message.errors[:user]).to include("を入力してください")
+        end
+      end
+
+      context "does not have neither an user_id" do
+        let(:group_id) { nil }
+
+        it "is invalid with message without group_id" do
+          message.valid?
+          expect(message.errors[:group]).to include("を入力してください")
+        end
+      end
     end
   end
 end
