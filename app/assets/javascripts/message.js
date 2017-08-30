2fd3330f610279501f174823a440ef32d03d129b
LAPTOP-KUD45KG1\naomi <nao.miwa.1112@gmail.com>
2017-08-30 21:41:41 +0900

    sending-js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,40 @@
+$(function(){
+  function buildHTML(message){
+    if (message.image != null){
+    var image = `<img src=${ message.image }>`;
+    } else {
+      image = '';
+    }
+    if (message.body != null){
+    var body = `${ message.body }`;
+    } else {
+      body = '';
+    }
+    var html = `<div class="main__message__up"><div class="main__message__up__name">${ message.user_name }</div><div class="main__message__up__date">${ message.time }</div></div><div class="main__message__low">${ body }</div><div class="main__message__low">${ image }</div><br>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    console.log(this)
+    var formData = new FormData(this);
+    var href = "../messages.json";
+    $.ajax({
+      url: href,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.main__messages').append(html);
+      $('#input-body').val('');
+      console.log("done");
+    })
+    .fail(function() {
+      alert('メッセージを入力してください');
+    })
+  })
+});
+
