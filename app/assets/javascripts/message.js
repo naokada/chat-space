182819909ad0831d486c075415f565101e5bde0f
LAPTOP-KUD45KG1\naomi <nao.miwa.1112@gmail.com>
2017-09-07 02:35:31 +0900

    finalcommit(消えた文のコミット)

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,27 @@
 $(document).on('turbolinks:load', function(){
+  setInterval(update, 5000);
+
+  function update(){
+    $.ajax({
+      url: location.href.json,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(json) {
+      var id = $('.main__message__up:last').data('id');
+      var insertHTML = '';
+      json.messages.forEach(function(message) {
+         if (message.id > id ) {
+          insertHTML += buildHTML(message);
+         }
+        });
+      $('.main__messages').append(insertHTML);
+      var bottomHeight = $('.main__messages')[0].scrollHeight;
+      $('.main').animate({ scrollTop: bottomHeight }, 'slow');
+    })
+  }
+
   function buildHTML(message){
     if (message.image != null){
     var image = `<img src=${ message.image }>`;
@@ -13,6 +36,7 @@ $(document).on('turbolinks:load', function(){
     var html = `<div class="main__message__up"><div class="main__message__up__name">${ message.user_name }</div><div class="main__message__up__date">${ message.time }</div></div><div class="main__message__low">${ body }</div><div class="main__message__low">${ image }</div><br>`
     return html;
   }
+
   $('#new_message').on('submit', function(e){
     $('.flash').html("").removeClass('flash__alert');
     e.preventDefault();
@@ -39,7 +63,8 @@ $(document).on('turbolinks:load', function(){
       var html = buildHTML(data);
       $('.main__messages').append(html);
       $form[0].reset();
-      return false;
+      var bottomHeight = $('.main__messages')[0].scrollHeight;
+      $('.main').animate({ scrollTop: bottomHeight }, 'slow');
     })
     .fail(function() {
       $('.flash').html("メッセージを入力してください").addClass('flash__alert');
